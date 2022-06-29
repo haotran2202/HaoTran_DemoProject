32c76e801863b1f9568e557cf4d8685760cb2519
haotran2202 <tphao2202@gmail.com>
2022-06-29 16:32:52 +0700

    Test : Add new test cases - SauceLabs: Sorting

---
+++ src/main/java/pageObjects/SauceLabs/LoginPageObject.java
@@ -0,0 +1,26 @@
+package pageObjects.SauceLabs;
+
+import commons.BasePage;
+import org.openqa.selenium.WebDriver;
+import pageUIs.SauceLabs.LoginPageUI;
+
+
+public class LoginPageObject extends BasePage {
+	 WebDriver driver;
+
+	public LoginPageObject(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public void sendkeyToUserNameTextbox(String userName){
+		sendKeyToElement(driver, LoginPageUI.USER_NAME_TEXTBOX, userName);
+	}
+
+		public void sendkeyToPasswordTextbox(String password){
+		sendKeyToElement(driver, LoginPageUI.PASSWORD_TEXTBOX, password);
+	}
+
+			public void clickToLoginButton(){
+		clickToElement(driver, LoginPageUI.LOGIN_BUTTON);
+	}
+}
---
+++ src/main/java/pageUIs/SauceLabs/LoginPageUI.java
@@ -0,0 +1,10 @@
+package pageUIs.SauceLabs;
+
+public class LoginPageUI {
+
+	public static final String USER_NAME_TEXTBOX = "//input[@id='user-name']";
+
+	public static final String PASSWORD_TEXTBOX = "//input[@id='password']";
+
+	public static final String LOGIN_BUTTON = "//input[@id='login-button']";
+}
